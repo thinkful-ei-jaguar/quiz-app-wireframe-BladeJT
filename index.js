dd46cc02637817317a88c06c054a85ee8d36fa2c
JT Liner <[jtliner925@gmail.com]>
2019-12-05 19:24:44 -0600

    added function stubs

--- index.js
+++ index.js
@@ -1,8 +1,50 @@
-// startQuiz -to start submit begin quiz
+'use strict';
+const STORE = [
 
+]
+
+
+// startQuiz -to start submit begin quiz
+function startQuiz(){
+
+};
 // Question page - Select answer radio button - required
+function selectAnswer(){
+
+}
 //disable other answers when submit answer
+
 //submit - submit answer button - will tell you correct answer and give fact to support
+function submitAnswer(){
+
+}
+
+function showCorrectAnswer(){
+
+}
+//keep track of score
+function keepScore(){
+
+}
 //submit next question button takes you to next question or final results page
+
 // shows results gives opportunity to take quiz again
+function showResults(){
+
+}
 //retake quiz will take you back to start page.
+function retakeQuiz(){
+
+}
+
+function handleQuizResults(){
+    startQuiz();
+    selectAnswer();
+    submitAnswer();
+    showCorrectAnswer();
+    keepScore();
+    showResults();
+    retakeQuiz();
+}
+
+$(handleShoppingList);
