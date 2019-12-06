71a748c2bdce6f8747518603a539dc238a98aca1
JT Liner <[jtliner925@gmail.com]>
2019-12-05 18:45:26 -0600

    added js and started user stories

---
+++ index.js
@@ -0,0 +1,8 @@
+// startQuiz -to start submit begin quiz
+
+// Question page - Select answer radio button - required
+//disable other answers when submit answer
+//submit - submit answer button - will tell you correct answer and give fact to support
+//submit next question button takes you to next question or final results page
+// shows results gives opportunity to take quiz again
+//retake quiz will take you back to start page.
